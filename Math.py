0e5cb850da283cafb7667532bfa62b8ad8b5ca32
unknown <mamohamini@gmail.com>
2023-08-10 18:36:01 -0700

    Type checked

--- Math.py
+++ Math.py
@@ -1,6 +1,8 @@
 # I need to implement this:
 def add(x,y):
-  return x+y
+    if (type(x)== int and type(y)== int:
+        return x+y
+    return None
 
 #Bob needs to implement this:
 def mult(x,y):
